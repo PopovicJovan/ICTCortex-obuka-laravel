1600aa7be54c2a7ddf605616182c3b197a8803eb
PopovicJovan <jovanp.school06@gmail.com>
2024-03-19 16:28:55 +0100

    inicijalni kod za drugu sekciju kursa kroz koju ce biti pravljena platforma za knjige, do sad su prodjeni osnovni sql upiti kroz php i malo detaljnija kreiranja tabela i upotreba foreign key

---
+++ book-review/app/Models/Book.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%'. $title .'%');
+    }
+
+    public function scopePopular(Builder $query, $from=null, $to=null): Builder
+    {
+        return $query->withCount(['reviews' =>
+                                   fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+                                 ])
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews)
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    public function scopeHighestRated(Builder $query, $from, $to): Builder
+    {
+        return $query->withAvg(['reviews' =>
+                                fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)],
+                         'rating')
+                ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangeFilter(Builder $q, $from, $to): void
+    {
+        if ($from && !$to) {
+            $q->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $q->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $q->whereBetween('created_at', [$from, $to]);
+        }
+    }
+}
+
