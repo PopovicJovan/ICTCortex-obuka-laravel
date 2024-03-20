66dcc34e1e9f715427bd00971181c61d594ef25b
PopovicJovan <jovanp.school06@gmail.com>
2024-03-20 15:52:15 +0100

    Dodata je forma za filtriranje knjiga po naslovu kao i filtriranje po popularnosti i ocjenama, sledece ce se razraditi logika u vezi toga

--- book-review/resources/views/books/index.blade.php
+++ book-review/resources/views/books/index.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Books</h1>
+    <h1 class="mb-10 text-2xl">Books</h1>
+
+    <form method="GET" action="{{route('books.index')}}" class="mb-5 flex items-center space-x-2">
+        <input type="text" name="title"
+               placeholder="Search by title"
+               value="{{request('title')}}"
+                class="input"/>
+        <button type="submit" class="btn">Submit</button>
+        <a class="btn " href="{{route('books.index')}}">Clear</a>
+    </form>
+
+    <div class="mb-4 filter-container flex">
+        @php
+            $filters = [
+                    '' => 'Latest',
+                    'popular_last_month' => 'Popular Last Month',
+                    'popular_last_6months' => 'Popular Last 6 Months',
+                    'highest_rated_last_month' => 'Highest Rated Last Month',
+                    'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+                ];
+        @endphp
+
+
+        @foreach($filters as $key => $label)
+            <a href="{{ route('books.index', ['filter' => $key]) }}"
+               class="{{request('filter') === $key || (is_null(request('filter')) && $key === '' ) ? "filter-item-active" : "filter-item"}}">
+                {{$label}}
+            </a>
+        @endforeach
+    </div>
+
+
     <form>
         <ul>
             @forelse($books as $book)
