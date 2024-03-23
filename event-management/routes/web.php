988ebebc8bc88f10a9e68a42fea0bd3bdceb951f
PopovicJovan <jovanp.school06@gmail.com>
2024-03-23 10:32:42 +0100

    Kreirane su tabele, modeli, factory i seeder za event i attendee

--- event-management/routes/web.php
+++ event-management/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+        ->scoped(['attendee' => 'event' ]);
