5e0383c9542535cdfc6d9d427af66175ab6739db
PopovicJovan <jovanp.school06@gmail.com>
2024-03-27 16:28:12 +0100

    Kreirana stranica koja prikazuje sve poslove sa svim detaljima koji idu uz posao

---
+++ job-board/resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+{{--<div class="rounded-md border border-slate-300 bg-white shadow-sm mb-4 p-4">--}}
+<div {{$attributes->class(["rounded-md border border-slate-300
+                            bg-white shadow-sm mb-4 p-4"])}}>
+    {{$slot}}
+</div>
--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\JobController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('', fn() => to_route('jobs.index'));
+Route::resource('jobs', JobController::class)->only(['index']);
