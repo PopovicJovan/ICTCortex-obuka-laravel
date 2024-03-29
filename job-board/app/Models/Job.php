45aceef4e8c1e1b51b921f191f7fd8e86a78eeb1
PopovicJovan <jovanp.school06@gmail.com>
2024-03-29 11:44:38 +0100

    U Employer dodat foreign id sa Users i u Job dodat foreign id sa Employer i db seeder je promijenjen

--- job-board/app/Models/Job.php
+++ job-board/app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -13,6 +14,11 @@ class Job extends Model
 
     public static array $experience = ['Entry', 'Intermediate', 'Senior'];
     public static array $category = ['IT', 'Marketing', 'Finance', 'Sales'];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
     public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
     {
