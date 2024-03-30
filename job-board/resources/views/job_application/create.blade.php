ad5c0611297c1409ca81dcafd7af44f915368377
PopovicJovan <jovanp.school06@gmail.com>
2024-03-30 17:06:59 +0100

    Omoguceno slanje cv-a u pdf formi pri apliciranju za posao

--- job-board/resources/views/job_application/create.blade.php
+++ job-board/resources/views/job_application/create.blade.php
@@ -5,7 +5,7 @@
         <h2 class="mb-4 text-lg font-medium">
             Your Job Application
         </h2>
-        <form action="{{route('job.application.store', $job)}}" method="POST">
+        <form action="{{route('job.application.store', $job)}}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="mb-4">
                 <label for="expected_salary"
@@ -13,6 +13,10 @@
                     Expected Salary
                 </label>
                 <x-text-input name="expected_salary" type="number"/>
+            </div>
+            <div class="mb-4">
+                <label class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+                <x-text-input type="file" name="cv"></x-text-input>
             </div>
             <x-button class="w-full">Apply!</x-button>
         </form>
